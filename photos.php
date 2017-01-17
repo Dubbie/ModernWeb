0607de63f456cb786762dbe079bbec999b79af24
Proksa Levente <proksalevente@gmail.com>
2017-01-18 00:32:42 +0100

    Added photos.php to repo

---
+++ photos.php
@@ -0,0 +1,18 @@
+<?php
+  $active = 'photos';
+  $title = 'Photos';
+  include './inc/header.php';
+  
+?>
+  <h1>photos</h1>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+<!--  <script src="./js/main.js"></script>-->
+  <?php include './inc/footer.php'; ?>
